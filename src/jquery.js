8c577bea92b2c372ac23d8967a0faa887452b3f8
adellanno <anthony@dellanno.co.uk>
2015-09-30 16:25:00 +0100

    working on checkbox

--- src/jquery.js
+++ src/jquery.js
@@ -4,16 +4,29 @@ $(document).ready(function()
    therm = new Thermostat();
 
    $('#output').html(therm.temp);
-   // document.getElementById('#output')
-
-   // $('#output').html('Hello World');
-   // $('#temp_up').click(function(event) {
-   //
-   // });
 
    $('#temp_up').click(function() {
      therm.increaseByOne();
      $('#output').html(therm.temp);
    });
 
+   $('#temp_down').click(function() {
+     therm.decreaseByOne();
+     $('#output').html(therm.temp);
+   });
+
+   $('#powerSave').prop('checked', true); {
+     therm.turnOffPowerSavingMode();
+   };
+
+    // $("#powerSave").click(function() {
+    //    .toggle(therm.turnOffPowerSavingMode)
+    // });
+  //     therm.turnOnPowerSavingMode();
+  // else
+  //     therm.turnOffPowerSavingMode();
+
+
  });
+    // $('#powerSave').prop('checked', true) {
+    //   therm.turnOnPowerSavingMode();
