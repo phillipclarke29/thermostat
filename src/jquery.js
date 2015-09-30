e5a0196be5869533c2da17b4bda992e471598a9b
adellanno <anthony@dellanno.co.uk>
2015-09-30 15:45:05 +0100

    temperature is updating on browser

--- src/jquery.js
+++ src/jquery.js
@@ -1,13 +1,19 @@
 $(document).ready(function()
 
-  {
-    therm = new Thermostat();
+ {
+   therm = new Thermostat();
 
-    $('#output').html(therm.temp);
-    // document.getElementById('#output')
+   $('#output').html(therm.temp);
+   // document.getElementById('#output')
 
-    // $('#output').html('Hello World');
-    // $('#temp_up').click(function(event) {
-    //
-    // });
-  });
+   // $('#output').html('Hello World');
+   // $('#temp_up').click(function(event) {
+   //
+   // });
+
+   $('#temp_up').click(function() {
+     therm.increaseByOne();
+     $('#output').html(therm.temp);
+   });
+
+ });
