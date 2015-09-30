dbd0dd2093e6d0fe606cbbc90f2e7715dd0adae2
philip <philip.clarke@redhood.co.uk>
2015-09-30 17:04:22 +0100

    power saving button working

--- src/jquery.js
+++ src/jquery.js
@@ -1,32 +1,38 @@
 $(document).ready(function()
 
- {
-   therm = new Thermostat();
+  {
+    therm = new Thermostat();
 
-   $('#output').html(therm.temp);
+    $('#output').html(therm.temp);
 
-   $('#temp_up').click(function() {
-     therm.increaseByOne();
-     $('#output').html(therm.temp);
-   });
+    $('#temp_up').click(function() {
+      therm.increaseByOne();
+      $('#output').html(therm.temp);
+    });
 
-   $('#temp_down').click(function() {
-     therm.decreaseByOne();
-     $('#output').html(therm.temp);
-   });
+    $('#temp_down').click(function() {
+      therm.decreaseByOne();
+      $('#output').html(therm.temp);
+    });
 
-   $('#powerSave').prop('checked', true); {
-     therm.turnOffPowerSavingMode();
-   };
+    $('input[type="checkbox"]').click(function() {
+      if ($("#powerSave").is(":checked")) {
+        therm.turnOnPowerSavingMode();
+      } else if ($("#powerSave").is(":not(:checked)")) {
+        therm.turnOffPowerSavingMode();
+      }
+      $('#output').html(therm.temp);
+    });
+    // };
 
     // $("#powerSave").click(function() {
     //    .toggle(therm.turnOffPowerSavingMode)
     // });
-  //     therm.turnOnPowerSavingMode();
-  // else
-  //     therm.turnOffPowerSavingMode();
+    //     therm.turnOnPowerSavingMode();
+    // else
+    //     therm.turnOffPowerSavingMode();
 
 
- });
-    // $('#powerSave').prop('checked', true) {
-    //   therm.turnOnPowerSavingMode();
+  });
+// $('#powerSave').prop('checked', true) {
+//   therm.turnOnPowerSavingMode();
